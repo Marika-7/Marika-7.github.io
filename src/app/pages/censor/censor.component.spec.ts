7ba86310419d16f2fd4c0141fa46e87a5c791ef7
Marika-7 <raduga.dva@gmail.com>
2022-02-15 19:03:29 +0200

    Lesson 5

---
+++ src/app/pages/censor/censor.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CensorComponent } from './censor.component';
+
+describe('CensorComponent', () => {
+  let component: CensorComponent;
+  let fixture: ComponentFixture<CensorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CensorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CensorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
