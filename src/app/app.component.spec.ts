10855a95a25c705a7e9cc5358365398337d7244e
Marika-7 <raduga.dva@gmail.com>
2022-02-24 12:29:40 +0200

    Lesson 6

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'task05'`, () => {
+  it(`should have as title 'task06'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('task05');
+    expect(app.title).toEqual('task06');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('task05 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('task06 app is running!');
   });
 });
--- src/app/task06/task06.component.spec.ts
+++ src/app/task06/task06.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TasklistComponent } from './tasklist.component';
+import { Task06Component } from './task06.component';
 
-describe('TasklistComponent', () => {
-  let component: TasklistComponent;
-  let fixture: ComponentFixture<TasklistComponent>;
+describe('Task06Component', () => {
+  let component: Task06Component;
+  let fixture: ComponentFixture<Task06Component>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TasklistComponent ]
+      declarations: [ Task06Component ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TasklistComponent);
+    fixture = TestBed.createComponent(Task06Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
